5d5753eaf154c3cf143dd1d079e8416cab2a71e8
Simon Cozens <simon@cpan.org>
2002-09-01 16:25:17 +0000

    Import of SIMON/Bundle-SDK-SIMON-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-SDK-SIMON
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         SIMON/Bundle-SDK-SIMON-1.00.tar.gz
    gitpan-cpan-author:       SIMON
    gitpan-cpan-maturity:     released

---
+++ SIMON.pm
@@ -0,0 +1,50 @@
+package Bundle::SDK::SIMON;
+
+$VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::SDK::SIMON - CPAN Bundle for '10 Modules I Wouldn't Go Anywhere Without'
+
+=head1 SYNOPSIS
+
+C<perl -MCPANPLUS -e 'install Bundle::SDK::SIMON'>
+
+=head1 CONTENTS
+
+Bundle::CPAN
+
+Bundle::LWP
+
+Mail::Send
+
+MLDBM
+
+Date::Calc
+
+DBI
+
+Data::Dumper
+
+POE
+
+File::Spec
+
+XML::Simple
+
+=head1 DESCRIPTION
+
+This bundle installs the 10 modules outlined in my popular "10 Modules I
+Wouldn't Go Anywhere Without" talk, given at TPC 5 and YAPC 2002. I'd
+highly recommend people who are beginning to use Perl modules to start
+with this essential set, which I consider to be a reasonable "Perl SDK".
+
+The talk notes are included in this distribution as F<modules.pod>
+
+=head1 AUTHOR
+
+Simon Cozens, C<simon@cpan.org>.
